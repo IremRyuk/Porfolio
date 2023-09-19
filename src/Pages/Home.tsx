01e269d1d3f778cdbcf828ebd41d9f91ff7fdf53
IremRyuk <IremRyuk>
2023-09-19 14:38:31 +0400

    add mobile sc project page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,17 @@
-// import Intro from "../Page Components/Home Page/Intro";
+import Intro from "../Page Components/Home Page/Intro";
 import PralaxPicAbout from "../Page Components/Home Page/PralaxPicAbout";
 import ProgrSkills from "../Page Components/Home Page/ProgrSkills";
 import Projects from "../Page Components/Home Page/Projects";
+import {useEffect} from 'react'
 
 export default function Home() {
+    // Set Page Top
+    useEffect(()=>{
+      window.scrollTo(0,0)
+    },[])
   return (
     <>
-    {/* <Intro /> */}
+    <Intro />
     <PralaxPicAbout />
     <ProgrSkills />
     <Projects />
--- src/Pages/ProjectDescr.tsx
+++ src/Pages/ProjectDescr.tsx
@@ -3,6 +3,7 @@ import '../Styles/ProjectDescr/descr.css'
 import Data from '../Storage/data.json'
 // Swiper Js
 import { Swiper, SwiperSlide } from 'swiper/react';
+import {useEffect} from 'react'
 
 import 'swiper/css';
 import 'swiper/css/effect-fade';
@@ -11,10 +12,14 @@ import 'swiper/css/pagination';
 
 // import required modules
 import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
-
 export default function ProjectDescr() {
+  // Filter Data iwth Params
     const {Id}:any = useParams()
-    const FilteredData:{id:number;names:string;image:string[]}|undefined = Data.find(res=>res.id === parseInt(Id))
+    const FilteredData:{id:number;names:string;image:string[];webSiteDescrition:string;websiteAdrress:string;myJob:string;skills:string;github?:string;}|undefined = Data.find(res=>res.id === parseInt(Id))
+  // Set Page Top
+    useEffect(()=>{
+      window.scrollTo(0,0)
+    },[])
   return (
     <>
     {FilteredData &&
@@ -45,6 +50,22 @@ export default function ProjectDescr() {
         ))}
     <p className="projectName">{FilteredData.names}</p>
     </Swiper>
+
+    {/* Project Description */}
+    <div className='description'>
+    <div className="leftText">
+      <p>Site Descrition: {FilteredData.webSiteDescrition}</p>
+      <center><div className='line'></div></center>
+      <p>My Job: {FilteredData.myJob}</p>
+      <center><div className='line'></div></center>
+      <p>WebSite Address: <a href={`${FilteredData.websiteAdrress}`} target="_blank" style={{color:'rgb(0 255 147)'}}>{FilteredData.websiteAdrress}</a></p>
+    </div>
+    <div className="rightSoc">
+      <p>Skills: {FilteredData.skills}</p>
+      <center><div className='line'></div></center>
+      <p>My Repositories: <a href={`${FilteredData.github}`} target="_blank" style={{color:'rgb(0 255 147)'}}>{FilteredData.github}</a></p>
+    </div>
+    </div>
     </div>
     </center>
     }
