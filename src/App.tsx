0d537e2f121f5dcd5d9ec5a944bc8df9f17340c0
IremRyuk <IremRyuk>
2023-09-23 19:15:22 +0400

    add nav

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from './Pages/Home'
 import './Styles/Defaults/App.css'
 import ProjectDescr from './Pages/ProjectDescr'
 import MoveTop from './Components/MoveTop'
+import Nav from './Components/Nav'
 // import CostumCursor from './Components/CostumCursor'
 
 
@@ -12,7 +13,7 @@ function App() {
       
       {/* Cursor */}
       {/* <CostumCursor /> */}
-
+<Nav />
       {/* Pages */}
       <MoveTop />
     <Routes>
---
+++ src/Components/Nav.tsx
@@ -0,0 +1,68 @@
+import {AppBar,Toolbar,Stack} from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import '../Styles/Nav/nav.css'
+import { useEffect, useState } from 'react'
+
+export default function Nav() {
+    const [bg,setBg] = useState<boolean>(true)
+    const navigate = useNavigate()
+    const AppBarMui1 = {
+        position:'fixed',
+        zInedx:'200',
+        top:'0%',
+        left:'0%',
+        width:'100vw',
+        height:'fit-content',
+        background:'transparent',
+        backdropFilter:'blur(15px)',
+        padding:'0px 5px',
+        boxShadow:'none'
+    }
+    const AppBarMui2 = {
+        position:'fixed',
+        zInedx:'200',
+        top:'0%',
+        left:'0%',
+        width:'100vw',
+        height:'fit-content',
+        background:'#2b2b2bcf',
+        padding:'0px 5px',
+        boxShadow:'none'
+    }
+    const BoxMuiTexts = {
+        width:'100%',
+        justifyContent:{xs:"space-between",sm:"space-around"},
+        alignItems:'center'
+    }
+    const changeBg = () => {
+        window.scrollY >=100?setBg(true):setBg(false)
+    }
+useEffect(()=>{
+    window.addEventListener('scroll',changeBg)
+})
+    
+  return (
+    <AppBar sx={bg?AppBarMui2:AppBarMui1}>
+      <Toolbar>
+<Stack direction='row' sx={BoxMuiTexts}>
+    {/* Profile */}
+<button className="profile-btn" onClick={()=>navigate('/')}>
+    Profile
+</button>
+{/* Contcts */}
+    <button className='con-btn' onClick={()=>navigate('/cotnacts')}>
+    CONTACT ME
+    <div id="clip">
+        <div id="leftTop" className="corner"></div>
+        <div id="rightBottom" className="corner"></div>
+        <div id="rightTop" className="corner"></div>
+        <div id="leftBottom" className="corner"></div>
+    </div>
+    <span id="rightArrow" className="arrow"></span>
+    <span id="leftArrow" className="arrow"></span>
+</button>
+</Stack>
+      </Toolbar>
+    </AppBar>
+  )
+}
