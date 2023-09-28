dd39b1384ed40fe3ab0c1591211f7fe6b92f31fe
IremRyuk <IremRyuk>
2023-09-28 20:07:18 +0400

    relaod function add

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,16 +2,34 @@ import Intro from "../Page Components/Home Page/Intro";
 import PralaxPicAbout from "../Page Components/Home Page/PralaxPicAbout";
 import ProgrSkills from "../Page Components/Home Page/ProgrSkills";
 import Projects from "../Page Components/Home Page/Projects";
-import { useEffect } from "react";
+import { useState,useEffect } from "react";
 
 export default function Home() {
+    // For Intro Page
+    const [is,setIs] = useState<boolean>(false)
+
     // Set Page Top
     useEffect(()=>{
+
+      // clear session storage on reloading
+      window.onbeforeunload = function(){sessionStorage.removeItem('intro')}
+
+      // move page top
       window.scrollTo(0,0)
+
+      // set sessionStorage
+      const getInfo = sessionStorage.getItem('intro')
+      if(getInfo === null){
+        setTimeout(()=>{
+          sessionStorage.setItem('intro','true')
+        },3000)
+      }else{
+        setIs(true)
+      }
     },[])
   return (
     <>
-    <Intro />
+    {is?null:<Intro />}
     <PralaxPicAbout />
     <ProgrSkills />
     <Projects />
