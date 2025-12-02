7597c36a91c6d66a57d73cc271073e8ae89814d4
iremryuk <iremryuk>
2025-12-02 17:35:09 +0400

    fix bug

--- src/Page Components/Home Page/Projects.tsx
+++ src/Page Components/Home Page/Projects.tsx
@@ -19,15 +19,15 @@ export default function Projects() {
         'My Projects'
         }
       </p></center>
-    {/* SEO */}
-    <center><p className='proj-web'>SEO</p></center>
-    <center><div className='projects'>
-{Seo.map((res:{id:number;names:string;image:string[]})=><ProjectImgsSEO main={res} key={res.id}/>)}
-    </div></center>
     {/* Content Creator */}
     <center><p className='proj-web'>Content Creator</p></center>
     <center><div className='projects'>
 {Cc.map((res:{id:number;names:string;image:string[]})=><ProjectImgsCC main={res} key={res.id}/>)}
+    </div></center>
+    {/* SEO */}
+    <center><p className='proj-web'>SEO</p></center>
+    <center><div className='projects'>
+{Seo.map((res:{id:number;names:string;image:string[]})=><ProjectImgsSEO main={res} key={res.id}/>)}
     </div></center>
     {/* Web Development */}
     <center><p className='proj-web'>Web Development</p></center>
